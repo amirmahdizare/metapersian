d3862789133fdaac3527b0463b5aea515e7f85ea
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-29 13:54:20 +0330

    Create Select Language Component

---
+++ app/_components/SideMenu/components/SelectLanguage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const SelectLanguage = () => {
+    return (
+        <div className='flex flex-row gap-2 items-center justify-between'>
+
+            <div className='flex flex-row gap-2 items-center'>
+                @
+                <span>زبان</span>
+                ^
+            </div>
+        </div>
+    )
+}
