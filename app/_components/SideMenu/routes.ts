d36bb4736c04e0a458c3ccab834f53cf20c2734b
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-29 13:53:40 +0330

    Add Menu Routes

--- app/_components/SideMenu/routes.ts
+++ app/_components/SideMenu/routes.ts
@@ -1,60 +1,69 @@
 import { MenuItemType } from "@/app/_types";
 import participation from '@/app/_images/menuImages/participation.png'
 import reporter from '@/app/_images/menuImages/reporter.png'
+import citizen from '@/app/_images/menuImages/citizen-baguette.png'
+import developer from '@/app/_images/menuImages/developer-baguette.png'
+import inspector from '@/app/_images/menuImages/citizen-baguette.png'
+import bussiness from '@/app/_images/menuImages/businessman-baguette.png'
+import lawyer from '@/app/_images/menuImages/lawyer-baguette.png'
+import cityCouncil from '@/app/_images/menuImages/city-council-baguette.png'
+import mayor from '@/app/_images/menuImages/the-mayor-baguette.png'
+import governer from '@/app/_images/menuImages/governor-baguette.png'
+import miniester from '@/app/_images/menuImages/minister-baguette.png'
 
 export const routes: Array<MenuItemType> = [
     {
         faTitle: 'شهروند',
-        img: participation
+        img: citizen
     },
     {
         faTitle: 'خبرنگار',
         img: reporter
     },
-    // {
-    //     faTitle:'مشارکت کننده',
-    //     img:''
-    // },
-    // {
-    //     faTitle:'توسعه دهنده',
-    //     img:''
-    // }
-    // ,
-    // {
-    //     faTitle:'بازرس',
-    //     img:''
-    // },
+    {
+        faTitle:'مشارکت کننده',
+        img:participation
+    },
+    {
+        faTitle:'توسعه دهنده',
+        img:developer
+    }
+    ,
+    {
+        faTitle:'بازرس',
+        img:inspector
+    },
 
-    // {
-    //     faTitle:'تاجر',
-    //     img:''
-    // },
-
-
-    // {
-    //     faTitle:'وکیل',
-    //     img:''
-    // },
-
-    // {
-    //     faTitle:'شورای شهر',
-    //     img:''
-    // },
-
-    // {
-    //     faTitle:'شهردار',
-    //     img:''
-    // },
+    {
+        faTitle:'تاجر',
+        img:bussiness
+    },
 
 
-    // {
-    //     faTitle:'استاندار',
-    //     img:''
-    // },
+    {
+        faTitle:'وکیل',
+        img:lawyer
+    },
 
-    // {
-    //     faTitle:'وزیر',
-    //     img:''
-    // },
+    {
+        faTitle:'شورای شهر',
+        img:cityCouncil
+    },
+
+    {
+        faTitle:'شهردار',
+        img:mayor
+    },
+
+
+    {
+        faTitle:'استاندار',
+        img:governer
+    },
+
+    {
+        faTitle:'وزیر',
+        img:miniester
+    },
 
 ]
