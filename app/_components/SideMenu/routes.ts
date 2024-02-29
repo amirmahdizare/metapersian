7e8713d632755e67dbab855aff9e006e9073558f
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-29 13:23:23 +0330

    Add  Menu Routes

---
+++ app/_components/SideMenu/routes.ts
@@ -0,0 +1,60 @@
+import { MenuItemType } from "@/app/_types";
+import participation from '@/app/_images/menuImages/participation.png'
+import reporter from '@/app/_images/menuImages/reporter.png'
+
+export const routes: Array<MenuItemType> = [
+    {
+        faTitle: 'شهروند',
+        img: participation
+    },
+    {
+        faTitle: 'خبرنگار',
+        img: reporter
+    },
+    // {
+    //     faTitle:'مشارکت کننده',
+    //     img:''
+    // },
+    // {
+    //     faTitle:'توسعه دهنده',
+    //     img:''
+    // }
+    // ,
+    // {
+    //     faTitle:'بازرس',
+    //     img:''
+    // },
+
+    // {
+    //     faTitle:'تاجر',
+    //     img:''
+    // },
+
+
+    // {
+    //     faTitle:'وکیل',
+    //     img:''
+    // },
+
+    // {
+    //     faTitle:'شورای شهر',
+    //     img:''
+    // },
+
+    // {
+    //     faTitle:'شهردار',
+    //     img:''
+    // },
+
+
+    // {
+    //     faTitle:'استاندار',
+    //     img:''
+    // },
+
+    // {
+    //     faTitle:'وزیر',
+    //     img:''
+    // },
+
+]
---
+++ app/_types/index.ts
@@ -0,0 +1,6 @@
+import { StaticImageData } from "next/image";
+
+export interface MenuItemType {
+    faTitle: string,
+    img: StaticImageData
+}
