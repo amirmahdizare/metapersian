ee33ca194667a1598688b3213356a41934c508d7
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-29 17:40:08 +0330

    Add Fonts

---
+++ app/_fonts/AzarMehr/index.ts
@@ -0,0 +1,110 @@
+import localFont from 'next/font/local'
+
+export const AzarMehr= localFont({
+    src: [
+
+        {
+            path: './word/AzarMehr-Thin.ttf',
+            weight: '200'
+        },
+        {
+            path: './word/AzarMehr-ExtraLight.ttf',
+
+            weight: '200'
+        },
+        {
+            path: './word/AzarMehr-Light.ttf',
+            weight: '300'
+        },
+        {
+            path: './word/AzarMehr-Regular.ttf',
+            weight: '400'
+        },
+        {
+            path: './word/AzarMehr-Regular.ttf',
+            weight: '500'
+        },
+        {
+            path: './word/AzarMehr-SemiBold.ttf',
+            weight: '600'
+        },
+        {
+            path: './word/AzarMehr-Bold.ttf',
+            weight: '700'
+        },
+
+        {
+            path: './word/AzarMehr-ExtraBold.ttf',
+            weight: '800'
+        },
+        
+        {
+            path: './word/AzarMehr-Black.ttf',
+            weight: '900'
+        },
+
+
+        {
+            path: './word/AzarMehr-ExtraBlack.ttf',
+            weight: '950'
+        },
+
+
+    ],
+    variable: '--font-azarmehr'
+})
+
+export const AzarMehrNumber= localFont({
+    src: [
+
+        {
+            path: './digit/AzarMehr-FD-Thin.ttf',
+            weight: '200'
+        },
+        {
+            path: './digit/AzarMehr-FD-ExtraLight.ttf',
+
+            weight: '200'
+        },
+        {
+            path: './digit/AzarMehr-FD-Light.ttf',
+            weight: '300'
+        },
+        {
+            path: './digit/AzarMehr-FD-Regular.ttf',
+            weight: '400'
+        },
+        {
+            path: './digit/AzarMehr-FD-Regular.ttf',
+            weight: '500'
+        },
+        {
+            path: './digit/AzarMehr-FD-SemiBold.ttf',
+            weight: '600'
+        },
+        {
+            path: './digit/AzarMehr-FD-Bold.ttf',
+            weight: '700'
+        },
+
+        {
+            path: './digit/AzarMehr-FD-ExtraBold.ttf',
+            weight: '800'
+        },
+        
+        {
+            path: './digit/AzarMehr-FD-Black.ttf',
+            weight: '900'
+        },
+
+
+        {
+            path: './digit/AzarMehr-FD-ExtraBlack.ttf',
+            weight: '950'
+        },
+
+
+    ],
+
+    variable: '--font-iranyekan'
+})
