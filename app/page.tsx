c6d0df25f30cb72a751add53f47ee19e2df7f27e
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-29 13:55:17 +0330

    Add Side Menu To Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import { SideMenu } from "./_components/SideMenu/SideMenu";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-row">
+      <SideMenu />
     </main>
   );
 }
