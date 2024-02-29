4fbfda9ab9ae532d2be98d9b4490dcdcda4202e5
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-02-29 17:43:30 +0330

    Set Font To Vazir Matn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,23 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import {  Vazirmatn } from "next/font/google";
 import "./globals.css";
-import { AzarMehr, AzarMehrNumber } from "./_fonts/AzarMehr";
-
 
 export const metadata: Metadata = {
-  title: "Metaverse Rang",
+  title: "Metaverse Rang Project",
 };
+
+const vazir = Vazirmatn({subsets:['arabic']})
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  
   return (
     <html lang="en" dir="rtl">
-      <body className={`${AzarMehr.className} ${AzarMehrNumber.className} font-medium text-[14px] container max-w-[1920px]`}>{children}</body>
+      <body className={` ${vazir.className} font-medium text-[14px] container max-w-[1920px]`}>{children}</body>
     </html>
   );
 }
