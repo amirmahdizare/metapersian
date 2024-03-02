3b4db2989cf757bb0dc7222be55b003668076d73
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-02 18:42:32 +0330

    Change Features

--- app/levels/_components/Features/Features.tsx
+++ app/levels/_components/Features/Features.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 export const Features = () => {
     return (
-        <div className='flex flex-row gap-2'>
-            {features.map(item => <div className='flex flex-col  justify-center items-center gap-6 p-8 pt-16 rounded-app flex-1 bg-[#080807] duration-150 transition-all text-dark-Font top-0 hover:top-4 hover:border hover:border-yellow-500 hover:shadow relative'>
+        <div className='flex flex-row gap-16 justify-around'>
+            {features.map(item => <div className='flex flex-col  justify-center items-center gap-4 p-8 pt-16 rounded-app flex-1 bg-[#080807] duration-150 transition-all text-dark-Font top-0 hover:top-4 hover:border hover:border-yellow-500 hover:shadow relative'>
                 <div className=' w-[80px] lg:w-[120px] aspect-square rounded-app-curve bg-dark-on-bg p-2 text-center relative'>
                     <Image  src={item.img} alt={item.title} fill />
                 </div>
