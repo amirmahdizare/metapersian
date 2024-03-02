2130fb60cacdce8808a572ba9fe847286bb39d77
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-02 19:43:39 +0330

    Add Icons

--- app/_components/Detailtem/Detailtem.tsx
+++ app/_components/Detailtem/Detailtem.tsx
@@ -1,10 +1,22 @@
+import { Check, Close } from '@/app/_icons'
 import React from 'react'
 
-export const Detailtem = ({ title, value }: { title: string, value: string }) => {
+export const Detailtem = ({ title, value }: { title: string, value: any }) => {
+
+    const generateValue = () => {
+        if (typeof value == 'boolean' && !value)
+            return <Close width={25} height={25} />
+        else if (typeof value == 'boolean' && value)
+            return <Check width={25} height={25} />
+        else
+            return <span className='text-dark-tex-gray'>{value}</span>
+
+
+    }
     return (
         <div className='flex flex-row gap-2 justify-between p-2 border-b-4 border-b-dark-on-bg'>
             <span className='text-white'>{title}</span>
-            <span className='text-dark-tex-gray'>{value}</span>
+            {generateValue()}
         </div>
     )
 }
---
+++ app/_icons/Close.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Close = (props:React.SVGProps<SVGSVGElement>) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none" {...props}>
+            <path d="M13 1L1 13M1 1L13 13" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    )
+}
