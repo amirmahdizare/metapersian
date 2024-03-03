8edc2235cdd473132f590b01c3fc3069268728d9
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-03 20:21:24 +0330

    Add Footer App Logos

---
+++ app/levels/_components/Footer/components/Logos/data.mock.ts
@@ -0,0 +1,140 @@
+import { FooterLogoType } from "@/app/_types";
+import facebook from '@/app/_images/footerLogos/facebook.png'
+import rss from '@/app/_images/footerLogos/feed.png'
+import insta from '@/app/_images/footerLogos/instagram (2).png'
+import linkedin from '@/app/_images/footerLogos/linkedin.png'
+import pin from '@/app/_images/footerLogos/path (1).png'
+import whatsapp from '@/app/_images/footerLogos/whatsapp.png'
+import youtube from '@/app/_images/footerLogos/youtube.png'
+import rubica from '@/app/_images/footerLogos/rubika.png'
+import tel from '@/app/_images/footerLogos/telegram.png'
+import virgol from '@/app/_images/footerLogos/virgool.png'
+import bell from '@/app/_images/footerLogos/add.png'
+import aparat from '@/app/_images/footerLogos/aparat.png'
+import delfak from '@/app/_images/footerLogos/dalfak.png'
+import discord from '@/app/_images/footerLogos/discord.png'
+import faq from '@/app/_images/footerLogos/faq.png'
+import filo from '@/app/_images/footerLogos/filo.png'
+import jadeh from '@/app/_images/footerLogos/jabeh.png'
+import medium from '@/app/_images/footerLogos/medium.png'
+import mp4 from '@/app/_images/footerLogos/mp4.png'
+import namash from '@/app/_images/footerLogos/namasha.png'
+
+
+
+
+export const logos :Array<FooterLogoType> = [
+    {
+        img:facebook,
+        link:'#'
+    },
+
+
+    {
+        img:rss,
+        link:'#'
+    },
+
+
+    {
+        img:insta,
+        link:'#'
+    },
+
+
+    {
+        img:linkedin,
+        link:'#'
+    },
+
+
+    {
+        img:pin,
+        link:'#'
+    },
+
+
+    {
+        img:whatsapp,
+        link:'#'
+    },
+
+    {
+        img:youtube,
+        link:'#'
+    },
+
+    {
+        img:rubica,
+        link:'#'
+    },
+
+
+    {
+        img:tel,
+        link:'#'
+    },
+
+    {
+        img:virgol,
+        link:'#'
+    },
+
+    {
+        img:bell,
+        link:'#'
+    },
+
+    {
+        img:aparat,
+        link:'#'
+    },
+
+    {
+        img:delfak,
+        link:'#'
+    },
+
+    // {
+    //     img:rubica,
+    //     link:'#'
+    // },
+    {
+        img:discord,
+        link:'#'
+    },
+
+    {
+        img:faq,
+        link:'#'
+    },
+
+    {
+        img:filo,
+        link:'#'
+    },
+
+
+    {
+        img:jadeh,
+        link:'#'
+    },
+
+
+    {
+        img:medium,
+        link:'#'
+    },
+
+    {
+        img:mp4,
+        link:'#'
+    },
+
+    {
+        img:namash,
+        link:'#'
+    },
+
+
+]
