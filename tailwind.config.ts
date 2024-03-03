385a47854ea0a9ad22e7f9d3b932be1b97e4db98
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-03 19:02:07 +0330

    Fix 4 Components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode:'class',
+  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,29 +14,32 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors:{
-        'dark-active-btn':'#FFC700',
-        'dark-off-btn':'#C9C9C9',
-        'dark-Font':'#E1E1E1',
-        'dark-bg':'#000000',
-        'dark-tooltip':'#868B90',
-        'dark-hover':'#FDD136',
-        'dark-error':'#C30000',
-        'dark-error-input':'#ff00000',
-        'dark-on-bg':'#1A1A18',
-        'dark-success':'#00966D',
-        'dark-font-success':'#18C08F',
-        'dark-link':'#2a85ff',
-        'dark-tex-gray':'#C4C4C4'
+      colors: {
+        'dark-active-btn': '#FFC700',
+        'dark-off-btn': '#C9C9C9',
+        'dark-Font': '#E1E1E1',
+        'dark-bg': '#000000',
+        'dark-tooltip': '#868B90',
+        'dark-hover': '#FDD136',
+        'dark-error': '#C30000',
+        'dark-error-input': '#ff00000',
+        'dark-on-bg': '#1A1A18',
+        'dark-success': '#00966D',
+        'dark-font-success': '#18C08F',
+        'dark-link': '#2a85ff',
+        'dark-tex-gray': '#C4C4C4'
 
       },
-      borderRadius:{
-        'app':'10px',
-        'app-curve':'100px'
+      borderRadius: {
+        'app': '10px',
+        'app-curve': '100px'
+      },
+      boxShadow: {
+        'card': '2px 2px 20.700000762939453px 0px rgba(245, 245, 245, 0.3) '
       }
-    
+
     },
-    
+
   },
   plugins: [],
 };
