2b496dc02271c0fc48b0207d19e8230558ccc01c
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-05 21:16:32 +0330

    Develope Base Component

---
+++ app/levels/reporter/components/ButtonList.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+import React from 'react'
+
+export const ButtonList = () => {
+    return (
+        <Link href={'#'} className="p-3 rounded-app text-base bg-[#1A1A18] text-gray-400 ">لیست دریافت کنندگان</Link>
+    )
+}
---
+++ app/levels/reporter/components/ReporterName.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ReporterName = () => {
+  return (
+    <span className="text-2xl font-bold text-[#F1F3F5]">سطح خبرنگار یک</span>
+  )
+}
