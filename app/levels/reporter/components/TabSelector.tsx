e1c99423814cd3db5cfc513d3309c0a7eb911035
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-05 18:55:27 +0330

    Add Tab Selector

---
+++ app/levels/reporter/components/TabSelector.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+export const TabSelector = () => {
+
+
+    const pathname = usePathname()
+
+
+    const ButtonLink = ({ title, link }: { title: string, link: string }) => {
+        return <Link href={`/levels/reporter${link}`} className={pathname.includes(link) ? 'text-dark-active-btn border-b-2 border-dark-active-btn p-3 pb-2.5' : ' border-b-2 border-transparent p-3 pb-2.5'}>
+            {title}
+        </Link>
+    }
+
+
+    return (
+        <div className="bg-dark-on-bg rounded-app  flex justify-evenly whitespace-nowrap  flex-row gap-4 w-full text-[#868B90] overflow-auto">
+            <ButtonLink title="اطلاعات اولیه سطح" link="/info" />
+            <ButtonLink title="مجوز ها و دسترسی ها" link="/permissions" />
+            <ButtonLink title="نگین سطح" link="/gem" />
+            <ButtonLink title="هدیه همراه" link="/gift" />
+            <ButtonLink title="پاداش رسیدن به سطح" link="/reward" />
+        </div>
+    )
+}
--- app/levels/reporter/layout.tsx
+++ app/levels/reporter/layout.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { TabSelector } from "./components/TabSelector";
 
 
 export default function RootLayout({
@@ -14,13 +15,6 @@ export default function RootLayout({
     const pathname = usePathname()
 
 
-    const ButtonLink = ({ title, link }: { title: string, link: string }) => {
-        return <Link href={`/levels/reporter${link}`} className={pathname.includes(link) ? 'text-dark-active-btn border-b-2 border-dark-active-btn p-3 pb-2.5' : ' border-b-2 border-transparent p-3 pb-2.5'}>
-            {title}
-        </Link>
-    }
-
-
 
     return (
         <div className="flex flex-col gap-6 bg-[#080807] w-full box-border p-8  rounded-app flex-1  ">
@@ -29,13 +23,7 @@ export default function RootLayout({
                 <Link href={'#'} className="p-3 rounded-app bg-[#1A1A18] text-gray-400 lg:block hidden">لیست دریافت کنندگان</Link>
             </div>
 
-            <div className="bg-dark-on-bg rounded-app  flex justify-evenly whitespace-nowrap  flex-row gap-4 w-full text-[#868B90] overflow-auto">
-                <ButtonLink title="اطلاعات اولیه سطح" link="/info" />
-                <ButtonLink title="مجوز ها و دسترسی ها" link="/permissions" />
-                <ButtonLink title="نگین سطح" link="/gem" />
-                <ButtonLink title="هدیه همراه" link="/gift" />
-                <ButtonLink title="پاداش رسیدن به سطح" link="/reward" />
-            </div>
+            <TabSelector />
             <div className="text-xl">
                 {children}
             </div>
