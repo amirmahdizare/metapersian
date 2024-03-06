5fba20f96bba174fcdb189dd126f230d82b20754
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-06 23:21:08 +0330

    Add Click Away Listener To Popup Description

--- app/levels/_components/PopUpDescription/PopUpDescription.tsx
+++ app/levels/_components/PopUpDescription/PopUpDescription.tsx
@@ -1,13 +1,14 @@
 'use client'
 import { Close } from '@/app/_icons'
 import React, { ReactNode, useState } from 'react'
+import ClickAwayListener from 'react-click-away-listener'
 
 export const PopUpDescription = ({ description, children }: { description: string, children?: ReactNode }) => {
 
     const [open, setOpen] = useState<boolean>(false)
 
     return (
-        <div className='flex flex-col gap-4 items-start lg:hidden'>
+        <div className='flex flex-col gap-6 items-start lg:hidden'>
             <span className='text-white'>توضیحات:</span>
             <p className='line-clamp-6 text-ellipsis overflow-hidden text-white'>{description}</p>
 
@@ -17,13 +18,15 @@ export const PopUpDescription = ({ description, children }: { description: strin
             </div>
 
             {open && <div className='fixed top-0 left-0 backdrop-brightness-[25%] p-4 h-screen w-screen'>
-                <div className='rounded-app border-gray-400 p-8 flex flex-col gap-4 max-w-screen-sm border overflow-auto bg-dark-on-bg text-gray-400'>
-                    <div className='flex flex-row gap-2 justify-between'>
-                        <span className='text-2xl'>توضیحات :</span>
-                        <Close className='text-gray-400 cursor-pointer' width={17} height={17} onClick={()=>setOpen(false)} />
+                <ClickAwayListener onClickAway={() => setOpen(false)}>
+                    <div className='rounded-app border-gray-400 p-8 flex flex-col gap-4 max-w-screen-sm border overflow-auto bg-dark-on-bg text-gray-400'>
+                        <div className='flex flex-row gap-2 justify-between'>
+                            <span className='text-2xl'>توضیحات :</span>
+                            <Close className='text-gray-400 cursor-pointer' width={17} height={17} onClick={() => setOpen(false)} />
+                        </div>
+                        <p className='leading-8'>{description}</p>
                     </div>
-                    <p className='leading-8'>{description}</p>
-                </div>
+                </ClickAwayListener>
             </div>}
 
         </div>
