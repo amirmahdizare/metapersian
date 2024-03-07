36169f0a51c0778a1063a7ce3e98f7a9b8580b37
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-07 15:24:34 +0330

    Fix Features

--- app/(routes)/levels/_components/FooterSymbols/FooterSymbols.tsx
+++ app/(routes)/levels/_components/FooterSymbols/FooterSymbols.tsx
@@ -5,11 +5,11 @@ import Image from 'next/image'
 
 export const FooterSymbols = () => {
     return (
-        <div className='flex flex-row gap-4 bg-dark-on-bg p-2 rounded-app flex-wrap justify-evenly w-full'>
+        <div className='grid grid-cols-5 lg:flex flex-row gap-4 bg-dark-on-bg p-2 rounded-app flex-wrap justify-evenly w-full'>
 
 
-            {footerSymbols.reverse().map(item => <Link key={item.link} href={item.link} className='aspect-square relative  w-[30px] lg:w-[55px]'>
-                <Image src={item.img} alt={item.link}  />
+            {footerSymbols.reverse().map(item => <Link key={item.link} href={item.link} className='aspect-square relative col-span-1 object-cover  lg:w-[55px]'>
+                <Image src={item.img} alt={item.link} className='object-cover w-full aspect-square' />
             </Link>)}
 
         </div>
