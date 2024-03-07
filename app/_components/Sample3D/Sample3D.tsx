d4ee615dd8b7e137e220924f061cf8e21b2e813e
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-07 17:48:59 +0330

    Modify 3d Model

--- app/_components/Sample3D/Sample3D.tsx
+++ app/_components/Sample3D/Sample3D.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import React, { Suspense, useRef } from 'react'
-import { Canvas , useFrame, useThree } from '@react-three/fiber';
-import { OrbitControls, PerspectiveCamera, useFBX } from '@react-three/drei';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { OrbitControls, PerspectiveCamera, useFBX, OrthographicCamera } from '@react-three/drei';
 
 export const Sample3D = () => {
 
@@ -10,99 +10,43 @@ export const Sample3D = () => {
 
     const ref = useRef<any>();
 
+    const cameraRef = useRef<any>()
+
 
     const Box = () => {
 
-        useFrame(() => (ref?.current?.rotation?.y ? ref.current.rotation.y += 0.001 : undefined));
+        // useFrame(() => (ref?.current?.rotation?.y ? ref.current.rotation.y += 0.05 : undefined));
 
-        // Here, we can access the camera via the useThree hook
         useThree(({ camera }) => {
-            console.log(camera)
-            camera.position.y = 0;
-            camera.lookAt(0, 1.8, 0);
+            // return camera.rotateY(90)
+            // camera.position.y = 0;
+            // camera.lookAt(0, 1.8, 0)
+            // camera.lookAt(0, -200, 0)
         });
 
         return <primitive ref={ref} object={bugget} scale={4.5} />
     }
 
-    return <Canvas className="cursor-pointer border">
-        <ambientLight />
-        <OrbitControls enableZoom={false} 
-        maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2} 
+    return <Canvas className="cursor-pointer" >
+        <Suspense fallback={<span className='text-white '>Loading...</span>}>
+            <ambientLight />
+            <OrbitControls enableZoom={false}
+                // maxPolarAngle={Math.PI / 2} //To Curve
+                // minPolarAngle={Math.PI / 2} //To Curve
+                target={[-100, 180, 0]} //Key To Solve
+                enablePan
+                enableRotate
             />
-        <Suspense fallback={null}>
             <Box />
+            <OrthographicCamera
+                makeDefault
+                // fov={10}
+                // far={10}
+                // aspect={2/3}
+                position={[300, 100, 0]}
+                ref={cameraRef}
+            />
         </Suspense>
-        <PerspectiveCamera
-        makeDefault
-        // fov={10}
-        // modelViewMatrix={}
-        // far={10}
-        // near={1}
-        // position={[200, 200, 200]}
-        position={[300, 50, 0]} 
-        // position={[-200, 1500, 50]}
-        // position={[100,100,100]} 
-        // lookAt={() => (new Vector3(0, 0, 0))}
-        // rotation={[-45, 0, 0]}
-        />
     </Canvas>
 
-
-    return (
-        <Suspense fallback={<span className='text-white '>Loading...</span>}>
-
-            <Canvas className="cursor-pointer border border-gray-100 " frameloop="demand"
-                camera={{
-                    // position: [-4, 3, 6],
-                    // position: [300, 100, 0],
-                    // // position: [-8, 16, 0],
-                    // // position: [0, 0, 0],
-                    // // fov: 45,
-                    // fov: 30,
-                    // near: 0.1,
-
-
-                    // far: 200
-
-                }}
-                style={{ height: '100%', width: '100%' }}
-            >
-                <pointLight position={[10, 10, 10]} />
-                <OrbitControls
-                    // autoRotate
-                    // target={[0,0,0]}
-                    // enableZoom={true}
-                    maxPolarAngle={Math.PI / 2}
-                    minPolarAngle={Math.PI / 2}
-                    // minPolarAngle={0}
-                    // maxPolarAngle={45}
-                    // position={[0, 50, 30]}
-
-                    enableRotate
-                    enablePan={true}
-
-                />
-                <PerspectiveCamera
-                    makeDefault
-                    fov={10}
-
-
-                    // modelViewMatrix={}
-                    // far={10}
-                    // near={1}
-                    // position={[200, 200, 200]}
-                    // position={[50, -100, 200]} 
-                    position={[-200, 1500, 50]}
-                // position={[100,100,100]} 
-                // lookAt={() => (new Vector3(0, 0, 0))}
-                // rotation={[-45, 0, 0]}
-                />
-                {/* <boxGeometry args={[-100, 50, 100]} /> */}
-                <primitive ref={ref} object={bugget} scale={2.5} />
-                <ambientLight intensity={1} />
-            </Canvas >
-        </Suspense>
-    )
 }
