de407d6fca07edcd68c416be0907e71daeb2ac55
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-07 15:25:39 +0330

    Fix Spacing

--- app/(routes)/levels/reporter/info/page.tsx
+++ app/(routes)/levels/reporter/info/page.tsx
@@ -11,7 +11,7 @@ export default function page() {
 
 
     return (
-        <div className='grid grid-cols-3 gap-4 rounded-app2 bg-dark-section-color p-4'>
+        <div className='grid grid-cols-3 gap-6 rounded-app2 bg-dark-section-color p-4'>
 
             <div className=' col-span-3 lg:hidden order-1'><TabSelector /></div>
 
