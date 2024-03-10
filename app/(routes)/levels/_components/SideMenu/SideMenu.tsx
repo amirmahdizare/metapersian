431b4c36489ee36ffebd584d6a3e0854d0e0e1bd
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-10 23:15:39 +0330

    Fix Responsive Menu

--- app/(routes)/levels/_components/SideMenu/SideMenu.tsx
+++ app/(routes)/levels/_components/SideMenu/SideMenu.tsx
@@ -25,7 +25,7 @@ export const SideMenu = ({ isMobile }: { isMobile?: boolean }) => {
 
       {isMobile  && <Menu className='cursor-pointer text-gray-300' width={22.5} height={22.5} onClick={() => setExpanded(true)} />}
       
-      {(!isMobile  || expaned) && <div className={`bg-dark-on-bg z-20 ${isMobile ? 'lg:hidden' : ''} fixed right-0 top-0 backdrop-brightness-50 text-dark-tooltip text-[16px] h-screen  rounded-app  gap-4 flex transition-all duration-300 flex-col ease-linear  ${expaned ? ' max-w-[10000px] p-5 items-stretch  top-0' : 'max-w-[60px] lg:max-w-[100px] overflow -hidden p-5   justify-start items-center'}`}>
+      {(!isMobile  || expaned) && <div className={`bg-dark-on-bg z-20 ${isMobile ? 'lg:hidden' : ''} fixed right-0 top-0 backdrop-brightness-50 text-dark-tooltip text-[16px] h-screen h-dvh lg:h-screen  rounded-app  gap-4 flex transition-all duration-300 flex-col ease-linear  ${expaned ? ' max-w-[10000px] p-5 items-stretch  top-0' : 'max-w-[60px] lg:max-w-[100px] overflow -hidden p-5   justify-start items-center'}`}>
 
         {!expaned && <Menu width={20} height={20} className='cursor-pointer text-gray-300' onClick={() => setExpanded(!expaned)} />}
 
