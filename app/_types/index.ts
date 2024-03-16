aac0491d993b1cf488422d7fe7275d363f04ac3d
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-16 15:49:46 +0330

    Add Response Type

--- app/_types/index.ts
+++ app/_types/index.ts
@@ -21,3 +21,26 @@ export interface FooterLogoType {
     img: StaticImageData,
     link: string
 }
+
+export type pageProps<PT = {}, SP = {}> = {
+    params: {
+        [Property in keyof PT]: PT[Property];
+    },
+    searchParams: {
+        [Property in keyof SP]: SP[Property];
+    }
+}
+
+export type LevelPageProps = pageProps<{ params: [string, 'info' | 'gift' | 'permissions' | 'reward' | 'gem'] }>
+
+export interface LevelItemType {
+    id: string,
+    name: string,
+    slug: string,
+    image: string,
+    background_image: string,
+    png_file: string,
+    fbx_file: string,
+    gif_file: string,
+    description: string
+}
