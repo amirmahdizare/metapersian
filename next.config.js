065cf2f66cdb8300852865efbd02540f558f0bf9
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-16 15:49:05 +0330

    Change Next Config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: '**',
+            // port: '',
+            // pathname: '*',
+          },
+          {
+            protocol: 'https',
+            hostname: '**'
+            // port: '',
+            // pathname: '*',
+          },
+        ],
+      },
 };
 
 module.exports = nextConfig
